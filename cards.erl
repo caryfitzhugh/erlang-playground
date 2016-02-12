8e47b87d51299789d5e2747a57b6c94db25dab83
Cary FitzHugh <cary.fitzhugh@gmail.com>
2016-02-12 15:14:07 -0500

    Shuffled and dealt the cards'

--- cards.erl
+++ cards.erl
@@ -3,7 +3,8 @@
 %%
 -module(cards).
 -export([get_deck/0,
-         shuffle/1]).
+         shuffle/1,
+         deal_in/2]).
 
 get_deck() ->
   [{Suit, Num} || Suit <- [hearts, diamonds, clubs, spades], Num <- [ace,1,2,3,4,5,6,7,8,9,10,jack,queen,king]].
@@ -12,3 +13,27 @@ shuffle(Deck) ->
   lists:sort(fun(_,_) ->
                random:uniform() > 0.5
              end, Deck).
+
+create_hands( 0 ) -> [];
+create_hands( N ) when N > 0 -> [ [] | create_hands( N-1 ) ].
+
+deal([], Hands)  -> Hands;
+deal(Deck, Hands) when length(Deck) > 0 ->
+  %% You want to add one pull from the deck to each of the hands
+  %% and keep dealing.
+  {CardsForRound, Rest} = lists:split(length(Hands), Deck),
+
+  %% Add the takens to each of the Hands
+  {NewHands, _} = lists:mapfoldl(fun(Hand, CardRound) -> {[hd(CardRound) | Hand], tl(CardRound)} end, CardsForRound, Hands),
+
+  %% Keep dealing with the rest of the deck (if there are no more cards, it'll hit the base match
+  %% And just return "Hands"
+  deal(Rest, NewHands)
+  .
+
+
+%% Deal all the players into the game
+deal_in(Deck, Players) ->
+  EmptyHands = create_hands(Players),
+ deal(Deck, EmptyHands)
+  .
--- game.erl
+++ game.erl
@@ -1,8 +1,24 @@
-%% This is the game.
+%% This is the game. It is WAR
 %%
 
-c(cards).
+-module(game).
+-export([play/0]).
+-import(cards, [shuffle/1, get_deck/0, deal_in/2]).
 
-Deck = cards:get_cards().
+%%% Ask how many players would like to play... 2 - 52
+play() ->
+  Deck = cards:shuffle(cards:get_deck()),
+  {ok, [Players]} = io:fread("Welcome to WAR.  How many players would you like to involve? (2-52): ", "~d"),
+  io:format("Ok! Starting the game with ~B players~n", [Players]),
+  Hands = cards:deal_in(Deck, Players),
+  Winner = play_game(Hands),
+  io:format("Winner is Player #~B!!~n", [Winner])
+  .
 
-Shuffled = cards:shuffle(Deck).
+play_game(Hands) ->
+  1
+  .
+
+play_turn(Hands) ->
+  1
+  .
